7a57e1a5c20ba2524af637faefcef39071eef511
SkullEnemyX <vineetkishore01@gmail.com>
2019-06-03 09:14:33 +0000

    Removed main.dart file

--- NoteX/settings.py
+++ NoteX/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '%69n=bm7m4tyw)^)*x1xc4*lj7p(&3mqph0uc-bp62t=zy6^kq'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1','localhost','10.0.2.2']
+ALLOWED_HOSTS = ['127.0.0.1','localhost','10.0.2.2','10.0.2.15']
 
 
 # Application definition
